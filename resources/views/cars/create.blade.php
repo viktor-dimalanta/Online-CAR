d04cd220d82b90e99a82b288a38e13b0be635815
Regie Ebalobor <reg.ebalobor@kaisa.com>
2018-02-21 11:21:34 +0800

    Again

--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -5,7 +5,7 @@
         {{ csrf_field() }}
 
         <div class="bg-light lter b-b wrapper-md">
-            <h1 class="m-n font-thin h4">Originator's Window EDITED3</h1>
+            <h1 class="m-n font-thin h4">Originator's Window EDITED4</h1>
         </div>
 
         @include('layouts.partials._errors')
