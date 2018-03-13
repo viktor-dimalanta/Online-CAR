c1c02a97a537162caeb7e470222d0a73f978ad1b
PHR\tor.dimalanta <tor.dimalanta@kaisa.com>
2018-03-13 10:49:14 +0800

    title change 66

--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -5,7 +5,7 @@
         {{ csrf_field() }}
 
         <div class="bg-light lter b-b wrapper-md">
-            <h1 class="m-n font-thin h4">Originator's Window 5sample</h1>
+            <h1 class="m-n font-thin h4">Originator's Window 666</h1>
         </div>
 
         @include('layouts.partials._errors')
