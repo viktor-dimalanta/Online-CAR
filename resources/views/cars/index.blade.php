c7b749950b4e65e0004ebd4b883bc30fa15599cd
PHR\tor.dimalanta <tor.dimalanta@kaisa.com>
2018-03-15 10:23:02 +0800

    try descby id

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -53,7 +53,7 @@
                             </td>
                             <td class="text-right">
 
-                                {{$car->statuses()->first()->title}}
+                                {{$car->statuses()->sortyByDesc('id')->title}}
 
                             </td>
                             <td>
