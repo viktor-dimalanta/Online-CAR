aa9e3d78ad8c194c2e7b156135917b99aa0fb627
PHR\tor.dimalanta <tor.dimalanta@kaisa.com>
2018-03-16 13:23:01 +0800

    try output cars

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -4,7 +4,7 @@
     <div class="bg-light lter b-b wrapper-md">
         <h1 class="m-n font-thin h3">All CARs</h1>
     </div>
-
+    {{$cars}}
     <div class="wrapper-md">
 
         <div class="row">
@@ -42,7 +42,7 @@
                         </tr>
                         </thead>
                         <tbody>
-                          {{$cars}}
+
                         @foreach ($cars as $car)
                         <tr>
                             <td>{{ $car->id }}</td>
