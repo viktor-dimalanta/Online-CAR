ce368a85a6dfa90cdcfae91d1d850598c5c39f7a
PHR\tor.dimalanta <tor.dimalanta@kaisa.com>
2018-03-19 10:38:07 +0800

    change last

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -60,7 +60,7 @@
                             </td>
                             <td>
                                 <div class="progress progress-sm active m-t-xs m-b-none">
-                                    <div class="progress-bar {{ $car->statuses->last()->style}}" data-toggle="tooltip" data-original-title="100%" style="width: 100%"></div>
+                                    <div class="progress-bar {{ $car->statuses->first->style}}" data-toggle="tooltip" data-original-title="100%" style="width: 100%"></div>
                                 </div>
                             </td>
                             <td class="text-center">
