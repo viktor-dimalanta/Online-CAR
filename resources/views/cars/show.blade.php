b40e2e4db642d7592e08f35390acf62b4a70dd26
PHR\tor.dimalanta <tor.dimalanta@kaisa.com>
2018-03-19 16:53:51 +0800

    remove button

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -87,16 +87,7 @@
                             </div>
                         </div>
 
-                        <div class="col-sm-12">
-                            <div class="line line-dashed b-b line-lg pull-in"></div>
-                            <div class="form-group">
-                                <div class="col-sm-12 {{-- text-center--}} " style="display:none;">
-                                    <a href="{{ route('index') }}" class="btn btn-default">Cancel2</a>
-                                    <button type="submit" class="btn btn-warning" name="draft_button">Draft</button>
-                                    <button type="submit" class="btn btn-success" name="save">Submit CAR</button>
-                                </div>
-                            </div>
-                        </div>
+                        
 
                         {{--
 
